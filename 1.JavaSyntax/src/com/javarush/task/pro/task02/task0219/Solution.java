c48dd3df372b26bf7f591dd1b7dd059ee129998a
CodeHacker23 <ilarion0@yandex.ru>
2023-09-28 23:16:52 +0500

    add Solution JavaSyntax task0102 JavaSyntax task0109 Объявляли перменные0105 Баги робокопа0219 Комментарии излишни0107 5 задач 28.09.23

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int id = 15;
+        int age = 31;
+
+        String documents = " Vag ";
+        String  behavior = " Variator ";
+    }
+}
