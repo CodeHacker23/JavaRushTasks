c7ff3a929de8de6a4bb361045f5a3b4968bf4446
CodeHacker23 <ilarion0@yandex.ru>
2023-09-28 23:31:17 +0500

    add Solution JavaSyntax task0102 JavaSyntax task0109 Объявляли перменные0105 Баги робокопа0219 Комментарии излишни0107  task0201  6 задач 28.09.23

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…» 28/09/23
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        int intValue = 245;
+        int numericValue = 32;
+        int size = 12;
+        int number = 24;
+
+
+
+    }
+}
