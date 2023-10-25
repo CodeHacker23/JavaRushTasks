522215edd66f38530583bdbc7506be62ca73cee7
Квантор <ilarion0@yandex.ru>
2023-10-25 23:42:25 +0300

    add Solution JavaSyntax task0102 JavaSyntax task0109 Объявляли перменные0105 Баги робокопа0219 Комментарии излишни0107 коммит 25.10.23(2 задачки решить завтра)

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление 25.10.23
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner csr = new Scanner(System.in);
+        int a = csr.nextInt();
+        int b = csr.nextInt();
+        int c = csr.nextInt();
+
+         int sum = a * b *c;
+        System.out.println(sum);
+
+    }
+}
