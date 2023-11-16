a5d24738969f8a2a564390ab6936b7a4a590fca5
Квантор <ilarion0@yandex.ru>
2023-11-17 00:46:47 +0500

    add Solution JavaSyntax task03.task0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки 16.11.23 1 попыт сом
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+        int namberA = 100;
+        while (namberA != 0) {
+            System.out.println(  quote);
+            namberA --;
+        }
+
+    }
+}
