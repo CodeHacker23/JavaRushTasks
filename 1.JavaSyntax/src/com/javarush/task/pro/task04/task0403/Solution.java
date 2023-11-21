acb31727fff5da5d64105e5bc3bb73c4e6a8b46a
Квантор <ilarion0@yandex.ru>
2023-11-22 01:34:01 +0500

    add Solution JavaSyntax task03.task0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -3,26 +3,25 @@ package com.javarush.task.pro.task04.task0403;
 import java.util.Scanner;
 
 /* 
-Суммирование
+Суммирование с 11 попыт с помощью ии
 */
 
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
         Scanner csr = new Scanner(System.in);
-        int namberA = csr.nextInt();
         int sum = 0;
         String text = "ENTER";
 
         while (true) {
+             String a = csr.nextLine();
+             if(a.equals(text)){
+                 break;
+             }
+            int number = Integer.parseInt(a);
+            sum += number;
 
-                String a = csr.nextLine();
-                if (a.equals(text)) {
-                    break;
-                }
-                sum = sum + namberA;
-                namberA --;
-            }
+        }
 
         System.out.println(sum);
 
@@ -30,3 +29,6 @@ public class Solution {
 
     }
 }
+
+
+
