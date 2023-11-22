6c9dc1a85d2fe85d1f9233064ea6d14361f4ec20
Квантор <ilarion0@yandex.ru>
2023-11-23 01:09:57 +0500

    add Solution JavaSyntax task03.task0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник 22.11.23 1 попыт сом
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int h = 0;
+        String a = "Q";
+
+
+        while (h < 5) {
+            int m = 0;
+            while (m < 9){
+                System.out.print(a);
+                m++;
+            }
+            System.out.println(a);
+            h++;
+
+
+
+        }
+    }
+}
