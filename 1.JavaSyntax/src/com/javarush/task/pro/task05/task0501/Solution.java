ff1c7e2e3bec92d55046b24e0d9e5ed724c8091e
Квантор <ilarion0@yandex.ru>
2023-12-14 01:21:27 +0500

    add Solution JavaSyntax task0501

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task05.task0501;
+
+/* 
+Создаем массивы 13/12/23 1 попыт сом
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int [] intArray = new int[10];
+        double [] doubleArray = new double[10];
+
+    }
+}
